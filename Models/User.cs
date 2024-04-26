24fd04c2181e2f7e5868f771277a00565c78e21f
Sabaa-Abdennabi <saba.abdennebi@insat.ucar.tn>
2024-04-26 09:25:19 +0100

    added user auth

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Urlize_back.Models
+{
+    public class User :IdentityUser
+    {
+        public string? BusinessName { get; set; }
+        public string? Categories { get; set;}
+
+        public string? StudentCard { get; set; }
+    }
+}
